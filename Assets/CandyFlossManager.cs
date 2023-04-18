996908b170f71148e832fcf6d156395aef477998
Stavrin <steffanleprince@gmail.com>
2023-04-18 22:44:05 +0100

    Got the camera view in the right place for candy floss, adding eat candy floss.

--- Assets/CandyFlossManager.cs
+++ Assets/CandyFlossManager.cs
@@ -22,13 +22,28 @@ public class CandyFlossManager : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        floss.enabled = true;
-
-        Vector3 enlarge = new Vector3(0f, 0f, 0.2f);
-
-        floss.transform.localScale += enlarge * Time.deltaTime;
-
+        if (other.tag == "Make")
+        {
+            floss.enabled = true;
+            Vector3 enlarge = new Vector3(0f, 0f, 0.2f);
+            floss.transform.localScale += enlarge;
+        }
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(this.name + " collided with " + other.name);
+
+        if (other.tag == "Eat")
+        {
+            Vector3 shrink = new Vector3(0f, 0f, -1.0f);
+
+            floss.transform.localScale -= shrink;
+
+        }
+    }
+
+
     private void OnCollisionStay(Collision collision)
     {
 
